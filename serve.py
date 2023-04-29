ea3b5bde0f52c87c67a14cf66e8b9d98c017fd31
salvadormundi <ap7504685986@gmail.com>
2023-04-29 22:07:55 +0530

    updated http codes

--- serve.py
+++ serve.py
@@ -17,7 +17,7 @@ def extractive_summary():
         return {'summary': text_summarizer(text=split_sentences(plain_text), percentage=gay.get('percentage'))}, 200
     
     except (IndexError, ValueError, TypeError):
-        return {'message': 'Invalid Parameters'}, 500
+        return {'message': 'Invalid Parameters'}, 400
 
 
 @app.route("/abstractive", methods=['POST'])
