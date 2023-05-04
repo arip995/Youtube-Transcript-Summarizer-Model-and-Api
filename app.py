22f6c4631d67222777618563de5e672f296fda3b
salvadormundi <ap7504685986@gmail.com>
2023-05-04 13:26:10 +0530

    bando 2

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/extractive", methods=['GET', 'POST'])
+@app.route("/extractive", methods=['POST'])
 @cross_origin()
 def extractive_summary():
     try:
-        gay: Dict[Any] = request.get_json()
-        plain_text = APIv1(youtube_link=gay.get('link')).get_video_id().get_transcript()    # noqa: E501
-        return {'summary': text_summarizer(text=split_sentences(plain_text), percentage=gay.get('percentage'))}, 200    # noqa: E501
+        res: Dict[Any] = request.get_json()
+        plain_text = APIv1(youtube_link=res.get('link')).get_video_id().get_transcript()    # noqa: E501
+        return {'summary': text_summarizer(text=split_sentences(plain_text), percentage=res.get('percentage'))}, 200    # OK
     
     except (IndexError, ValueError, TypeError, ):
-        return {'message': 'Invalid Parameters'}, 400
+        return {'message': 'Invalid Parameters'}, 400   # bad request
     
     except err:
-        return {'message': 'No transcripts available for this video'}, 500
+        return {'message': 'No transcripts available for this video'}, 500  # Internal server error
 
 
-@app.route("/abstractive", methods=['GET', 'POST'])
+@app.route("/abstractive", methods=['POST'])
 @cross_origin()
 def abstractive_summary():
     return {'message': 'Not Implemented'}
